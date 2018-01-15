65192f053fd580280483a3ab96fb36dbe1f2fbe5
Radoslaw Kozlowski <radoslaw.kozlowski@valtech.co.uk>
2018-01-15 21:09:09 +0000

    Add Banners and Commerce projects

---
+++ src/Project/Common/code/Templates.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data;
+
+namespace SitecoreCoffee.Project.Common
+{
+  public struct Templates
+  {
+  }
+}
