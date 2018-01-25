fb9a1ac4e830cc1dfbb79c2c05ac7e65840577dd
Radoslaw Kozlowski <reo.fox@gmail.com>
2018-01-25 17:51:06 +0000

    Add ReplaceCart pipeline

---
+++ src/Feature/Commerce/code/Pipelines/Carts/ReplaceCart/ReplaceCart.cs
@@ -0,0 +1,45 @@
+﻿using Sitecore.Commerce;
+using Sitecore.Commerce.Pipelines;
+using Sitecore.Commerce.Services.Carts;
+using Sitecore.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace SitecoreCoffee.Feature.Commerce.Pipelines.Carts.ReplaceCart
+{
+    public class ReplaceCart : PipelineProcessor<ServicePipelineArgs>
+    {
+        public CartServiceProvider CartServiceProvider { get; protected set; }
+
+        public ReplaceCart(CartServiceProvider cartServiceProvider)
+        {
+            Assert.IsNotNull(cartServiceProvider, "cartServiceProvider");
+            this.CartServiceProvider = cartServiceProvider;
+        }
+
+        public override void Process(ServicePipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+
+            ReplaceCartRequest replaceCartRequest = (ReplaceCartRequest)args.Request;
+            CartResult cartResult = (CartResult)args.Result;
+
+            Assert.IsNotNull(replaceCartRequest, "request");
+            Assert.IsNotNull(cartResult, "result");
+
+            if (replaceCartRequest.FromCart.Lines.Count > 0)
+            {
+                var lines = replaceCartRequest.FromCart.Lines;
+                replaceCartRequest.ToCart.Lines = new ReadOnlyCollection<Sitecore.Commerce.Entities.Carts.CartLine>(lines);
+            }
+
+            replaceCartRequest.ToCart.Properties = new PropertyCollection();
+
+            foreach (var property in replaceCartRequest.FromCart.Properties)
+            {
+                replaceCartRequest.ToCart.Properties.Add(property);
+            }
+            
+            cartResult.Cart = replaceCartRequest.ToCart;
+        }
+    }
+}
---
+++ src/Feature/Commerce/code/Pipelines/Carts/ReplaceCart/ReplaceCartRequest.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Commerce.Entities.Carts;
+using Sitecore.Commerce.Services.Carts;
+using Sitecore.Diagnostics;
+
+namespace SitecoreCoffee.Feature.Commerce.Pipelines.Carts.ReplaceCart
+{
+    public class ReplaceCartRequest : CartRequest
+    {
+        public Cart FromCart { get; protected set; }
+
+        public Cart ToCart { get; protected set; }
+
+        public ReplaceCartRequest(Cart fromCart, Cart toCart)
+        {
+            Assert.ArgumentNotNull(fromCart, "fromCart");
+            Assert.ArgumentNotNull(toCart, "toCart");
+
+            this.ToCart = toCart;
+            this.FromCart = fromCart;
+        }
+    }
+}
