140d91381c8eb0f6fb286048b95015960a76c64b
Radoslaw Kozlowski <radoslaw.kozlowski@valtech.co.uk>
2018-01-30 23:50:29 +0000

    Add cart search using ajax calls

---
+++ src/Feature/Commerce/code/Frontend/js/cartSearch.js
@@ -0,0 +1,27 @@
+﻿var CartSearch = function() {
+
+    var ids = {
+        form: "#cart-search-form",
+        result: "#cart-search-result"
+    };
+
+    $(ids.form).submit(function(e) {
+        e.preventDefault();
+
+        var url = $(this).attr("action");
+        var data = $(this).serialize();
+
+        $.ajax({
+            url: url,
+            method: "POST",
+            data: data,
+            success: function (result) {
+                $(ids.result).html(result);
+            }
+        });
+    });
+}
+
+$(document).ready(function() {
+    document.cartSearch = CartSearch();
+});
