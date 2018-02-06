e0c51832301096c6559781b0b72e27d2f9c7d7f0
Radoslaw Kozlowski <reo.fox@gmail.com>
2018-02-06 17:29:31 +0000

    Add banners controller

---
+++ src/Feature/Banners/code/Controllers/BannersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SitecoreCoffee.Feature.Banners.Controllers
+{
+    public class BannersController : Controller
+    {
+        public ActionResult Static()
+        {
+            return View();
+        }
+    }
+}
