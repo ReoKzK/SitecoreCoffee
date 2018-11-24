3143cc826a3455ec2c3e59c2645894861e62de7f
Radoslaw Kozlowski <radoslaw.kozlowski@ethisys.co.uk>
2018-11-24 15:35:32 +0000

    Add extension method for retrieving current rendering properties

---
+++ src/Foundation/Extensions/code/SitecoreHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Mvc.Helpers;
+
+namespace SitecoreCoffee.Foundation.Extensions
+{
+    public static class SitecoreHelperExtensions
+    {
+        /// <summary>
+        /// Gets current rendering property
+        /// </summary>
+        /// <param name="sitecoreHelper">SitecoreHelper instance</param>
+        /// <param name="key">A key of property</param>
+        /// <returns>Property value</returns>
+        public static string GetRenderingProperty(this SitecoreHelper sitecoreHelper, string key)
+        {
+            string value = string.Empty;
+
+            var renderingContext = Sitecore.Mvc.Presentation.RenderingContext.CurrentOrNull;
+
+            if (renderingContext != null)
+            {
+                value = renderingContext.Rendering?.Properties[key];
+            }
+
+            return value;
+        }
+    }
+}
